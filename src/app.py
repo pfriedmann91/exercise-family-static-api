91297963b7d63e3461e7ed09b9e8b8f641c76f41
Pablo Friedmann <pabloifm91@gmail.com>
2025-01-10 20:50:55 +0000

    First Commit

--- src/app.py
+++ src/app.py
@@ -26,17 +26,50 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_members():
+        members = jackson_family.get_all_members()
+        if not members: 
+            return jsonify ({"error": "not found"}), 404
+        return jsonify(members), 200
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+        member= jackson_family.get_member(member_id)
+        if member:
+            return jsonify(member), 200
+        return jsonify ({"error": "not found"}), 404
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    new_member = request.get_json()
+    if not new_member or not all(key in new_member for key in ["id", "first_name", "age", "lucky_numbers"]):
+        return jsonify({"error": "Faltan datos. Se requieren 'id', 'first_name', 'age' y 'lucky_numbers'."}), 400
+    
+
+    if not isinstance(new_member["id"], int):
+        return jsonify({"error": "id debe seer un número"}), 400
+
+    if not isinstance(new_member["first_name"], str):
+        return jsonify({"error": "first_name debe ser un txeto"}), 400
 
 
-    return jsonify(response_body), 200
+    if not isinstance(new_member["age"], int) or new_member["age"] <= 0:
+        return jsonify({"error": "age debe ser un número"}), 400
+
+    if not isinstance(new_member["lucky_numbers"], list) or not all(isinstance(num, int) for num in new_member["lucky_numbers"]):
+        return jsonify({"error": "lucky_numbers debe ser un o unos números enteros"}), 400
+
+    jackson_family.add_member(new_member)
+    return jsonify({"message": "Miembro agregado"}), 200
+
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    deleted = jackson_family.delete_member(member_id)
+    if deleted:
+        return jsonify({"eliminado": True}), 200
+    return jsonify({"error": "not found"}), 404
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
